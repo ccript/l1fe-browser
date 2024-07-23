f16baa4f779b05e81df7df66e9273faedba4d89c
ccriptdev10 <ahtisham@cheetahagency.com>
2024-07-23 13:08:19 +0500

    results data

--- src/components/search/search-content/VideoContent.jsx
+++ src/components/search/search-content/VideoContent.jsx
@@ -16,14 +16,38 @@ const VIDEO_RESULTS = [
     date: "1 year ago",
     views: "1.2M",
   },
+  {
+    id: 2,
+    source: "YouTube > BBC News",
+    sourceLogo:
+      "https://imgs.search.brave.com/Ux4Hee4evZhvjuTKwtapBycOGjGDci2Gvn2pbSzvbC0/rs:fit:32:32:1/g:ce/aHR0cDovL2Zhdmlj/b25zLnNlYXJjaC5i/cmF2ZS5jb20vaWNv/bnMvOTkyZTZiMWU3/YzU3Nzc5YjExYzUy/N2VhZTIxOWNlYjM5/ZGVjN2MyZDY4Nzdh/ZDYzMTYxNmI5N2Rk/Y2Q3N2FkNy93d3cu/eW91dHViZS5jb20v",
+    videoThumbnail:
+      "https://imgs.search.brave.com/adJd_HAmCmwNe5FGAFfe5VIavMv334-ILcgzyJNebe8/rs:fit:200:200:1/g:ce/aHR0cHM6Ly9pLnl0/aW1nLmNvbS92aS9x/VVc2VC0xWWpQWS9t/YXhyZXNkZWZhdWx0/LmpwZw",
+    title: "Is AI an existential threat to human jobs? | BBC News",
+    desc: "As new roles are created and existing ones evolve, what will the future of work look like?...",
+    date: "1 year ago",
+    views: "1.2M",
+  },
+  {
+    id: 3,
+    source: "YouTube > BBC News",
+    sourceLogo:
+      "https://imgs.search.brave.com/Ux4Hee4evZhvjuTKwtapBycOGjGDci2Gvn2pbSzvbC0/rs:fit:32:32:1/g:ce/aHR0cDovL2Zhdmlj/b25zLnNlYXJjaC5i/cmF2ZS5jb20vaWNv/bnMvOTkyZTZiMWU3/YzU3Nzc5YjExYzUy/N2VhZTIxOWNlYjM5/ZGVjN2MyZDY4Nzdh/ZDYzMTYxNmI5N2Rk/Y2Q3N2FkNy93d3cu/eW91dHViZS5jb20v",
+    videoThumbnail:
+      "https://imgs.search.brave.com/adJd_HAmCmwNe5FGAFfe5VIavMv334-ILcgzyJNebe8/rs:fit:200:200:1/g:ce/aHR0cHM6Ly9pLnl0/aW1nLmNvbS92aS9x/VVc2VC0xWWpQWS9t/YXhyZXNkZWZhdWx0/LmpwZw",
+    title: "Is AI an existential threat to human jobs? | BBC News",
+    desc: "As new roles are created and existing ones evolve, what will the future of work look like?...",
+    date: "1 year ago",
+    views: "1.2M",
+  },
 ];
 const VideoContent = () => {
   return (
     <div className="w-full container mx-auto px-3 md:px-24 2xl:px-6 text-start py-2">
-      <div className="max-w-[630px] 2xl:max-w-[650px] border rounded-2xl p-5">
+      <div className="max-w-[630px] 2xl:max-w-[650px] border rounded-2xl">
         {VIDEO_RESULTS.map((video, index) => {
           return (
-            <div key={index} className="flex gap-4 py-2">
+            <div key={index} className="flex gap-4 p-5">
               <div className="min-w-[192px]">
                 <Image
                   width={192}
